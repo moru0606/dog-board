3de27330de880ea709fbae7109920813332216e2
hiroaki.ooki <moru150606@gmail.com>
2021-08-07 21:56:57 +0000

    feat: user機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
+  get 'signup', to: 'users#new'
+  resources :users
   resources :categories
   resources :posts do
     resources :comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_055629) do
+ActiveRecord::Schema.define(version: 2021_08_07_203320) do
 
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2021_08_07_055629) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
     t.integer "category_id"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "posts"
